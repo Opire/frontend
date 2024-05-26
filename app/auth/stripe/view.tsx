b8064dbb9d6f456edc39586005144fd5cec4c2f3
Rubén Rüger <ruben_27895@hotmail.com>
2024-05-26 17:21:55 +0200

    Dont call backend if any query param is not present in /auth/stripe

--- app/auth/stripe/view.tsx
+++ app/auth/stripe/view.tsx
@@ -11,20 +11,30 @@ export function AuthStripeView() {
     const searchParams = useSearchParams();
 
     useEffect(() => {
-        const code = searchParams.get("code") as string;
-        const ownerId = searchParams.get("state") as string;
+        const code = searchParams.get("code");
+        const ownerId = searchParams.get("state");
+
+        if (!code || !ownerId) {
+            router.push("/settings");
+            return;
+        }
+
         configureStripe({ code, ownerId });
     }, [searchParams]);
 
-    async function configureStripe({ code, ownerId }: { code: string, ownerId: string }) {
+    async function configureStripe({
+        code,
+        ownerId,
+    }: {
+        code: string;
+        ownerId: string;
+    }) {
         await clientCustomFetch(API_ROUTES.PAYMENTS.STRIPE_CONNECT_ACCOUNT(), {
             method: "POST",
             body: { code, ownerId },
         });
-        router.push('/settings')
+        router.push("/settings");
     }
 
-    return (
-        <LoadingOverlay visible={true}></LoadingOverlay>
-    );
+    return <LoadingOverlay visible={true}></LoadingOverlay>;
 }
