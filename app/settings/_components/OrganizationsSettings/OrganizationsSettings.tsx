a28672963d1ccf60eb3cb471336a0c2d61731a0d
Rubén Rüger <ruben_27895@hotmail.com>
2024-08-08 23:54:10 +0200

    Add info tip for organization customer portal

--- app/settings/_components/OrganizationsSettings/OrganizationsSettings.tsx
+++ app/settings/_components/OrganizationsSettings/OrganizationsSettings.tsx
@@ -1,8 +1,10 @@
 'use client'
-import { SimpleGrid } from "@mantine/core";
+import { Blockquote, SimpleGrid, Text } from "@mantine/core";
 import { FC } from "react";
 import { UserSettingsDTO } from "../../../_core/_dtos/UserSettingsDTO";
 import { OrganizationSettingsCard } from "./OrganizationSettingsCard";
+import { ORGANIZATION_TIER_NAMES } from "../../../_core/_types/TierNames";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 interface OrganizationsSettingsProps {
     organizations: UserSettingsDTO['payments']['organizations'];
@@ -11,10 +13,21 @@ interface OrganizationsSettingsProps {
 export const OrganizationsSettings: FC<OrganizationsSettingsProps> = ({
     organizations,
 }) => {
+    const hasAccessToSomeOrganizationCustomerPortal = organizations.some(organization => organization.tierName !== ORGANIZATION_TIER_NAMES.ORGANIZATION_BASIC)
 
     return (
-        <SimpleGrid cols={{ xs: 1, md: 2, lg: 3 }} w='100%' spacing='xl' verticalSpacing='xl' >
-            {organizations.map(organization => (<OrganizationSettingsCard key={organization.id} organization={organization} />))}
-        </SimpleGrid>
+        <>
+            {hasAccessToSomeOrganizationCustomerPortal &&
+                <Blockquote color='green' icon={<IconInfoCircle />}>
+                    <Text style={{ fontSize: '1.2rem' }}>
+                       To manage an organization subscription, you will need to provide the email address that was used when making the first payment of the subscription 
+                    </Text>
+                </Blockquote>
+            }
+            
+            <SimpleGrid cols={{ xs: 1, md: 2, lg: 3 }} w='100%' spacing='xl' verticalSpacing='xl' >
+                {organizations.map(organization => (<OrganizationSettingsCard key={organization.id} organization={organization} />))}
+            </SimpleGrid>
+        </>
     );
 }
