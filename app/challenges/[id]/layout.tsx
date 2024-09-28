022a957dff7c4c305e02af6c270464666fa5b933
Rubén Rüger <ruben_27895@hotmail.com>
2024-09-28 18:53:08 +0200

    Hide navbar in challenge pages

--- app/challenges/[id]/layout.tsx
+++ app/challenges/[id]/layout.tsx
@@ -1,5 +1,5 @@
 import { Container } from "@mantine/core";
-import { MainLayout } from "../../_components/Layouts/MainLayout";
+import { ChallengeLayout } from "../../_components/Layouts/ChallengeLayout/ChallengeLayout";
 
 export default function Layout({
     children,
@@ -7,10 +7,10 @@ export default function Layout({
     children: React.ReactNode
 }) {
     return (
-        <MainLayout>
+        <ChallengeLayout>
             <Container size='xxl' style={{ padding: '20px' }}>
                 {children}
             </Container>
-        </MainLayout>
+        </ChallengeLayout>
     )
 }
