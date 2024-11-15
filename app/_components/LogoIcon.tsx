4f2723f385859b66e761e728a0deee1e31c4fa73
Rubén Rüger <ruben_27895@hotmail.com>
2024-11-15 16:59:25 +0100

    Put size to Next Image

--- app/_components/LogoIcon.tsx
+++ app/_components/LogoIcon.tsx
@@ -8,13 +8,13 @@ import { FC } from "react";
 export const LogoIcon: FC = () => {
     const router = useRouter();
     const isMobile = useMediaQuery("(max-width: 50em)");
-    const logoSize = isMobile ? "26px" : "48px";
+    const logoSize = isMobile ? 26 : 48;
 
     function goToHome() {
         router.push("/");
     }
 
     return (
-        <Image src={"/opire_logo.svg"} alt="Opire logo" onClick={goToHome} style={{ cursor: "pointer", width: logoSize, height: logoSize }} />
+        <Image src={"/opire_logo.svg"} alt="Opire logo" width={logoSize} height={logoSize} onClick={goToHome} style={{ cursor: "pointer" }} />
     );
 };
